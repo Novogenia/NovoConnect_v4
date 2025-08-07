671ef9097433b7b5d06f0d8d11e22db74f760665
Novogenia <ceo@novogenia.com>
2025-08-07 10:02:59 +0200

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
-import { IconArrowRight } from "@tabler/icons-react"
-import { useTheme } from "next-themes"
-import Link from "next/link"
+import { ChatbotUISVG } from "@/components/icons/chatbotui-svg";
+import { IconArrowRight } from "@tabler/icons-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
 
 export default function HomePage() {
-  const { theme } = useTheme()
+  const { theme } = useTheme();
 
   return (
     <div className="flex size-full flex-col items-center justify-center">
@@ -18,11 +18,11 @@ export default function HomePage() {
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
-        href="/login"
+        href="/en/default/chat"
       >
         Start Chatting
         <IconArrowRight className="ml-1" size={20} />
       </Link>
     </div>
-  )
+  );
 }
